aae416dcb670a920a4e5680d5cbd00fb3e4921e4
Arpit <arpit.porwal@skilrock.com>
2018-02-28 18:19:28 +0530

    a

--- src/Words.java
+++ src/Words.java
@@ -83,7 +83,7 @@ public class Words {
 		wordMap.put("myriad", "a countless or great number of people or things");
 		wordMap.put("exalt", "think or speak very highly of(someone);raise to a higher rank");
 		
-		wordMap.put("notoriety", "being famous for something bad#infamous");
+		wordMap.put("notoriety", "being famous for something bad#infamous#egragious");
 		
 		wordMap.put("affront", "an action that causes offence(insult)");
 		wordMap.put("endear", "cause to be loved or liked");
@@ -99,7 +99,7 @@ public class Words {
 		wordMap.put("obscure", "not clearly expressed or easily understood;uncertain");
 		wordMap.put("prolific", "present in a large no. of quantities");
 		wordMap.put("heretic", "a person believing in or practising religious heresy");
-		wordMap.put("heresy", "contraty to orthodox");
+		wordMap.put("heresy", "contrary to orthodox");
 		wordMap.put("incendiary", "tending to stir up conflict;very exciting");
 		wordMap.put("smitten", "be strongly attracted to someone");
 		wordMap.put("disenfranchise", "deprive(someone) the right to vote");
@@ -107,8 +107,125 @@ public class Words {
 		wordMap.put("parsimonious", "very unwilling to spend money or an other resource");
 		wordMap.put("garrulous", "excessively talkative,expecially on trivial matters");
 		
+		//1th set
+		wordMap.put("swathe", "a broad strip or area of something");
+		wordMap.put("suburbia", "the suburbs");
+		wordMap.put("domicile", "treat a specified country as a permanent home");
+		wordMap.put("veritable", "give emphasis");
+		wordMap.put("bonanza", "a situation which creates sudden increase in wealth");
+		wordMap.put("necropolis", "a cementery, large one belonging to an ancient city");
+		wordMap.put("defection", "the desertion of one's country in favour of the other one");
+		wordMap.put("cementery", "a graveyard especially not in the church yard");
+		wordMap.put("haven", "a safe place to refuge");
+		wordMap.put("tantelize", "spur to action;tease someone over a thing that is unobtainable");
+		wordMap.put("ephemeral", "lasting a very sort time");
+		wordMap.put("posterity", "all future generations of people;all descendants");
+		wordMap.put("peripheral", "situated on the edge");
+		wordMap.put("conterminous", "having the same extent in space and time");
+		wordMap.put("messianic", "passionate#fervent");
+		wordMap.put("fervent", "passionate#messianic");
+		wordMap.put("gaggle", "a disorderly group of people");
+		wordMap.put("hush", "to make silent");
+		wordMap.put("reverent", "show solemn respect");
+		wordMap.put("alacrity", "eager to do something#solicitous");
+		wordMap.put("prosaic", "dull & lacking imagination");
+		wordMap.put("veracity", "truthful");
+		wordMap.put("voracity", "full of hunger");
+		wordMap.put("paucity", "lack of something");
+		wordMap.put("maintain", "keep stable");
+		wordMap.put("contrite", "remorseful");
+		wordMap.put("laconic", "a person of few words");
+		
+		//2nd set
+		wordMap.put("pugnacious", "aggressively argue about everything");
+		wordMap.put("disparate", "two things that are different from each other");
+		
+		wordMap.put("egregious", "standing out in a bad way#infamous#notoriety");
+		
+		wordMap.put("innocuous", "harmless & don't produce any ill-effects");
+		wordMap.put("candid", "straight forward and honest look at something");
+		wordMap.put("erratic", "sudden and unpredictable");
+		wordMap.put("bleak", "very depressing take on life");
+		wordMap.put("profuse", "pour out in abundance");
+		wordMap.put("extant", "opposite of extinct");
+		
+		wordMap.put("contentious", "likes to argue#contend");
+		wordMap.put("enervate", "to sap the energy from");
+		
+		wordMap.put("auspicious", "favourable");
+		wordMap.put("apocryphal", "a story of doubtful origin but widely circulated as being true");
+		wordMap.put("expatiate", "speak or write in detail about");
+		wordMap.put("complaint", "meeting in accordance with the rules and standards");
+		wordMap.put("inure", "accustom someone to something unpleasant");
+		
+		wordMap.put("parlous", "uncertain;full of danger#precarious");
+		
+		wordMap.put("edify", "morally uplift");
+		wordMap.put("mollify", "appease the anger of someone");
+		wordMap.put("solicitous", "eager to do something#alacrity");
+		wordMap.put("epithets", "adjective or phrase regarding the characteristic of a person");
+		wordMap.put("sprawling", "sit,lie,fall with open arms;spread out over a large area");
+		wordMap.put("picaresque", "relating to an episodic hero");
+		wordMap.put("heterodox", "not conforming with the accepted or orthodox standards");
+		wordMap.put("impotence", "unable to do anything");
+		wordMap.put("usurp", "take(a position of power) illigally, or by force");
+		
+		//3rd set
+		wordMap.put("tenuous", "very weak#slight");
+		wordMap.put("pristine", "in the original condition,unspoilt");
+		wordMap.put("obtuse", "annoyingly insensitive,slow to understand");
+		wordMap.put("desolate", "make a place appear bleakly empty,unhappy");
+		wordMap.put("moil", "to work hard");
+		wordMap.put("belligerent", "hostile & aggressive");
+		wordMap.put("corporeal", "relating to the body as opposed to the spirit");
+		wordMap.put("laud", "praise highly");
+		wordMap.put("procrastinate", "to put of doing something");
+		wordMap.put("acquit", "free from criminal charges#vindicate#absolve#exonerate");
+		wordMap.put("distraught", "very worried,upset");
+		wordMap.put("debiliate", "make very weak,hinder,delay");
+		wordMap.put("acute", "shrewd");
+		wordMap.put("eschew", "avoid#abstain");
+		wordMap.put("poignant", "sadness or regret;bad smell");
+		wordMap.put("trenchant", "vigorous or incisive in expression or style");
+		wordMap.put("iconoclast", "a person who attacks cherished beliefs");
+		wordMap.put("inveterate", "having a particular habit(long established);unlikely to change");
+		wordMap.put("meretricious", "apparently attractive but not having any real value");
+		wordMap.put("bicker", "argue about petty & trivial matters#squabble");
+		wordMap.put("equivocate", "to speak in ambiguous way");
+		wordMap.put("unequivocal", "state in such a way that there is no room for doubt");
+		wordMap.put("ambivalent", "conflicting feelings towards something");
+		wordMap.put("sedulous", "anything but idle;caring");
+		
 		//4th set
-		//wordMap.put("", "");
+		wordMap.put("blinkered", "limited outlook or understanding");
+		wordMap.put("unchecked", "something undesirable grown out of control");
+		wordMap.put("checkered", "marked with disreputable happenings");
+		wordMap.put("raft", "a large number of something");
+		wordMap.put("involved", "complicated");
+		wordMap.put("retiring", "shy;retract from a company");
+		wordMap.put("expansive", "socialable;wide-ranging");
+		wordMap.put("moment", "significant & important");
+		wordMap.put("base", "lower-class;without any moral principles");
+		wordMap.put("imbibe", "intake of knowledge or information");
+		wordMap.put("inundate", "to flood;overwhelmed by to many things#deluge");
+		wordMap.put("scintillating", "brilliant & lively");
+		wordMap.put("benighted", "state of ignorance");
+		wordMap.put("galvanize", "excite to action or spur on");
+		wordMap.put("hedge", "to limit,avoid making a direct statement");
+		wordMap.put("flush", "to turn red, to be in abundance,drive out of hiding");
+		wordMap.put("fell", "evil");
+		wordMap.put("arch", "deliberately teasing");
+		wordMap.put("tender", "to offer it up");
+		wordMap.put("intimate", "sugest something subtly");
+		wordMap.put("wanting", "lacking");
+		wordMap.put("becoming", "appropriate");
+		wordMap.put("start", "to dart in a particular direction");
+		wordMap.put("fleece", "to deceive");
+		wordMap.put("telling", "revealing of another factor");
+		wordMap.put("jingoism", "arouse patriotic emotions");
+		wordMap.put("cupidity", "extreme greed for material wealth");
+		wordMap.put("restive", "standing still;unable to remain silent due to boredom");
+		wordMap.put("augury", "an event telling what important events are to come");
 		return wordMap;
 	}
 	
